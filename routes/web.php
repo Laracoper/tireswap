911e29599f0b8604a670148d34e43df961adaffc
laracoper <laracoper26@gmail.com>
2026-04-21 23:23:18 +0300

    upgrade project

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
-// 1. Публичные роуты
+
 // Route::view('/', 'welcome');
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
-// В файле routes/web.php
+
 // Volt::route('/', 'welcome')->name('home');
 // Route::view('/', 'welcome')->name('home');
 
@@ -16,17 +16,14 @@ Route::get('/', function () {
 
 Volt::route('/wheels', 'pages.wheels.index')->name('wheels.index');
 
-// 2. Только для гостей
 Route::middleware('guest')->group(function () {
     Volt::route('/login', 'pages.auth.login')->name('login');
     Volt::route('/register', 'pages.auth.register')->name('register');
 });
 
-// 3. Только для авторизованных
 Route::middleware('auth')->group(function () {
     Route::view('/dashboard', 'dashboard')->name('dashboard');
-    
-    // Сначала статичные пути
+
     Volt::route('/wheels/create', 'pages.wheels.create')->name('wheels.create');
     // Volt::route('/messages', 'pages.messages.index')->name('messages.index');
     // Volt::route('/messages', 'msg')->name('messages.index');
